c8757ee96505c209baa86347f2ecae6a90df635f
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-07-13 21:28:09 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello\n")
+		io.WriteString(w, r.Method)
+	}
+
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
 }
